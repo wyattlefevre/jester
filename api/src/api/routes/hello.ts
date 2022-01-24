5a27937936a4263b8645288ecd8dfe87305c3241
Wyatt <wyattlefevre@byu.edu>
2022-01-24 16:00:54 -0700

    lots of updates

--- api/src/api/routes/hello.ts
+++ api/src/api/routes/hello.ts
@@ -4,6 +4,6 @@ const route = Router();
 export default (app: Router) => {
   app.use('/', route);
   route.get('/hello', (req: Request, res: Response) => {
-    return res.json({message: "hello"}).status(200)
+    return res.json("hello from jester api").status(200)
   })
 }
--- api/src/app.ts
+++ api/src/app.ts
@@ -15,6 +15,11 @@ async function startServer() {
     console.error(err)
     process.exit(1)
   })
+  // TODO: add socket.io
+  // https://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen
+  // https://socket.io/get-started/chat
+  // https://socket.io/docs/v3/server-application-structure/
+  // https://www.youtube.com/watch?v=ZKEqqIO7n-k&ab_channel=WebDevSimplified
 }
 
 startServer()
