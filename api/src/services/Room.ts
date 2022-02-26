3a2a105d515e9b07b3a6db066a99f9635923aac1
Wyatt <wyattlefevre@byu.edu>
2022-02-26 09:32:14 -0700

    more progress

--- api/src/services/Room.ts
+++ api/src/services/Room.ts
@@ -2,26 +2,50 @@ import { Socket } from 'socket.io'
 import { GameInstance } from './GameInstance'
 
 class Room {
-  private host: Socket[]
-  private players: Socket[]
+  private host: Socket
+  private players: Map<string, Socket> // nickname mapped to player Socket
   private game: GameInstance
   private playerLimit: number
   private roomCode: string
+  private lobbyOpen: boolean
   constructor(game: GameInstance, playerLimit: number, roomCode: string) {
     this.game = game
     this.playerLimit = playerLimit
-    this.players = []
+    this.players = new Map<string, Socket>()
     this.roomCode = roomCode
+    this.lobbyOpen = false // will be set to true once host has connected
   }
 
-  addPlayer(socket: Socket) {}
-  addHost(socket: Socket) {}
+  addPlayer(socket: Socket, nickname: string) {
+    if (!this.host) {
+      throw new RoomError("Host not connected")
+    }
+    if (!this.lobbyOpen) {
+      throw new RoomError("Lobby not open")
+    }
+    if (this.players.size >= this.playerLimit) {
+      throw new RoomError("Room is full")
+    }
+    if (this.players.has(nickname)) {
+      throw new RoomError("Nickname already in use")
+    }
+    this.players[nickname] = socket
+    socket.to(this.host.id).emit("player join", nickname)
+  }
+  addHost(socket: Socket) {
+    this.host = socket
+    this.lobbyOpen = true
+  }
   removePlayer(socket: Socket) {}
   getRoomCode(): string {
     return this.roomCode
   }
+  startGame() {
+    // TODO: logic to determine if starting a game is allowed
+    this.lobbyOpen = false
+  }
+  
 }
-
 export class RoomError extends Error {
   constructor(message: string) {
     super(message)
