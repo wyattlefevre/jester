9b8bfa500afcbf68220e7e0f4079f5255b1dbf7f
Wyatt <wyattlefevre@byu.edu>
2022-02-26 12:28:05 -0700

    sockets are connecting

--- react-client/src/pages/GameRoom.tsx
+++ react-client/src/pages/GameRoom.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { io, Socket } from 'socket.io-client'
+import { useAuth } from '../components/auth/AuthProvider'
 
 const GameRoom = () => {
   const { roomId } = useParams()
+  const [socket, setSocket ]= useState<Socket | null>()
+  const auth = useAuth()
+  // TODO: attempt to connect here
+  useEffect(() => {
+    const newSocket = io("http://localhost:8000")
+    newSocket.on("connect", () => {
+      newSocket.emit('join-room-as-host', roomId, auth.user?.getUsername())
+      setSocket(newSocket)
+    })
+    return (() => {
+      newSocket.disconnect()
+    })
+  }, [])
+  
 
   return <div>GameRoom {roomId}</div>
 }
